8284eded88346d1f7eae2872dda5d48597883609
Beryl Kerubo <berylben966@gmail.com>
2023-11-13 21:40:45 +0300

    dayy2

--- Day2.js
+++ Day2.js
@@ -1,9 +1,10 @@
 // code that prints the first 10 multiples of a number
 
-// number 5
-
-let num = 5;
-
-for (let i = 1; i <= 10; i++) {
-    console.log(num * i);
+const inputNumber = parseInt(prompt("Enter a number:"));
+if (inputNumber <= 0) {
+    alert("Error, please enter a positive number.");
+}else {
+    for (let i = 1; i <= 10; i++) {
+        console.log(inputNumber * i);
+    }
 }
