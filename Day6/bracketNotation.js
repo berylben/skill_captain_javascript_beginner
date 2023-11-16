d133d0f214eb2cffbb3fdf41979c7f6abae5b8ed
Beryl Kerubo <berylben966@gmail.com>
2023-11-16 17:02:55 +0300

    day6

---
+++ Day6/bracketNotation.js
@@ -0,0 +1,13 @@
+// using bracket notation to display objects that represent a book
+const book = {
+    title: "Quantum Mechanics",
+    author: "Q.O.T Mayaka",
+    year: 2023,
+    isAvailable: true
+};
+
+// output
+console.log("Title:", book["title"]);
+console.log("Author:", book["author"]);
+console.log("Year:", book["year"]);
+console.log("Is available:", book["isAvailable"]);
---
+++ Day6/dotNotation.js
@@ -0,0 +1,13 @@
+// using bracket notation to display objects that represent a book
+const book = {
+    title: "Atomic Habits",
+    author: "James Clear",
+    year: 2018,
+    isAvailable: true
+};
+
+// output
+console.log("Title:", book.title);
+console.log("Author:", book.author);
+console.log("Year:", book.year);
+console.log("IS available:", book.isAvailable);
